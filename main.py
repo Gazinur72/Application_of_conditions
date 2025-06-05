5a496a08aa1a4b9ac1d19189b3c6ddfb234de66f
Gaz <karimovgaz72@gmail.com>
2025-06-05 20:15:48 +0500

    Изменил метод для подсчета суммы всех четных чисел от 1 до 100, и убрал дубляж принтов в конец программы.

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-from itertools import count
+sum_of_even_numbers = sum([number for number in range(1, 101) if number % 2 == 0])
 
-sum_of_even_numbers = 0
-
-for number in range(1, 101):
-    if number %2 == 0:
-        sum_of_even_numbers += number
-
-print("Сумма всех четных чисел от 1 до 100:", sum_of_even_numbers)
-
-squares_of_odd_numbers = [i**2 for i in range(1,11) if i % 2 !=0]
-print("Квадраты всех нечетных чисел от 1 до 10:", squares_of_odd_numbers)
+squares_of_odd_numbers = [i ** 2 for i in range(1, 11) if i % 2 != 0]
 
 count = 0
 while True:
     number = float(input("Введите число: "))
     if number < 0:
         break
-    count +=1
-print(f"Количество введеных чисел : {count}")
+    count += 1
+
+print(f"Сумма всех четных чисел от 1 до 100: {sum_of_even_numbers},"
+      f" Квадраты всех нечетных чисел от 1 до 10: {squares_of_odd_numbers} "
+      f"Количество введеных чисел : {count}")
