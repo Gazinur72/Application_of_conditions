f8b664e48d3c6c82138bc6254e7b3e79685d1d6d
Gaz <karimovgaz72@gmail.com>
2025-06-05 20:57:37 +0500

    Добавил обработчик исключений .

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ sum_of_even_numbers = sum([number for number in range(1, 101) if number % 2 == 0
 
 squares_of_odd_numbers = [i ** 2 for i in range(1, 11) if i % 2 != 0]
 
-count = 0
-while True:
-    number = float(input("Введите число: "))
-    if number < 0:
-        break
-    count += 1
+try:
+    count = 0
+    while True:
+        number = float(input("Введите число: "))
+        if number < 0:
+            break
+        count += 1
+except ValueError:
+    print("Ошибка: введено число не соответствующее параметрам ввода!!!")
 
 print(f"Сумма всех четных чисел от 1 до 100: {sum_of_even_numbers},"
       f" Квадраты всех нечетных чисел от 1 до 10: {squares_of_odd_numbers} "
